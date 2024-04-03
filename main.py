210159403a88f93cb7411056d2e708c57209644c
Iñigo Quintana <quintanadinigo@gmail.com>
2024-04-03 10:11:02 -0600

    .

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import pygame 
+import cv2
+import mediapipe as mp
+import numpy as np
+import pygame
+import threading
 import sys
 
 from module.gameManager import GameManager
 from module.player import Player
 from module.ball import Ball
+
 
 #Inicializar la librería
 pygame.init()
@@ -31,9 +36,60 @@ ball.get_ball_start(windowSize)
 p1.get_player_start(windowSize)
 p2.get_player_start(windowSize)
 
-# Music
-pygame.mixer.music.load("module/resource/Veo en ti la luz.mp3")
-pygame.mixer.music.play(-1)
+
+# Inicializar la captura de video con OpenCV
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+# Configurar el tamaño deseado para la pantalla
+desired_width = 100
+desired_height = 100
+cap.set(3, desired_width)
+cap.set(4, desired_height)
+
+# Inicializar mediapipe para el seguimiento de manos
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=4,
+    min_detection_confidence=0.6
+)
+
+
+def hand_tracking_thread():
+     while True:
+          ret, frame = cap.read()
+          if not ret:
+               break
+
+          height, width, _ = frame.shape
+          frame = cv2.flip(frame, 1)
+          frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+          # Procesar las manos
+          results = hands.process(frame_rgb)
+
+          # Dibujar las manos
+          if results.multi_hand_landmarks is not None:
+               for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                         frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
+                    )
+
+          # Convertir el frame de OpenCV a formato Pygame
+          frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+          frame = np.rot90(frame)
+          frame = pygame.surfarray.make_surface(frame)
+          frame = pygame.transform.flip(frame, True, False)
+
+          # Mostrar el frame en la ventana de Pygame
+          screen.blit(frame, (0, 0))
+          pygame.display.update()
+          
+
+# Create the hand tracking thread
+hand_tracking_thread = threading.Thread(target=hand_tracking_thread)
+hand_tracking_thread.start()
 
 
 first_round = True
@@ -46,7 +102,7 @@ while True:
           if event.type == pygame.QUIT:
                sys.exit()
 
-          #! Eventos teclado
+          #* Eventos teclado
           # Al presionar tecla
           if event.type == pygame.KEYDOWN:
                pressed_key.add(event.key)
@@ -63,6 +119,7 @@ while True:
           # Permite eventos mientras sacan
           time_elapsed = 0
           screen.fill(BLACK)
+
           while time_elapsed < 1200: # 1000 milisegundos (1 segundo)
                game_manager.draw_game()
                game_manager.get_event()
@@ -71,8 +128,13 @@ while True:
           first_round = False
 
           
+     # Music
+     if pygame.K_v in pressed_key: 
+          pygame.mixer.music.load("module/resource/Veo en ti la luz.mp3")
+          pygame.mixer.music.play(-1)
      
-     # --- Zona de animación --- 
+     
+     #* --- Zona de animación --- 
      # Actualizar las velocidades en el bucle principal del juego
      p1.update_player_speed(pressed_key)
      p2.update_player_speed(pressed_key)
@@ -91,7 +153,7 @@ while True:
      game_manager.collision_ball()
 
 
-     # --- Zona de dibujo ---
+     #* --- Zona de dibujo ---
      game_manager.draw_game()
 
 
